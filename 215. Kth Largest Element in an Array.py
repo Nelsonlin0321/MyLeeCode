ae1b73d9b73e2a2e1893e12255df45b803338181
LIN Jieqin <34851732+Nelsonlin0321@users.noreply.github.com>
2021-03-06 18:43:53 +0800

    Update 215. Kth Largest Element in an Array.py

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -1,4 +1,3 @@
-# solution one
 class Solution(object):
     
     def swap_func(self,array,i,j):
@@ -45,7 +44,7 @@ nums = [3,2,1,5,6,4]
 k = 2
 KthLargest = solution.findKthLargest(nums, k)
 
-# solution two
+
 class Solution(object):
     
     def partition_func(self,nums, left, right):
@@ -84,3 +83,64 @@ class Solution(object):
         right = len(nums)-1
         k= (len(nums) - 1) -(k-1)
         return self.recursive_partition(nums,left,right,k)
+
+
+# untime: 116 ms, faster than 22.44% of Python online submissions for Kth Largest Element in an Array.
+# Memory Usage: 14.6 MB, less than 17.70% of Python online submissions for Kth Largest Element in an Array.
+
+class Solution(object):
+    
+    def heapify(self,nums,list_len,parent_index):
+        left_index = 2 *parent_index+1
+        right_index = left_index+1
+        max_index =  parent_index
+        if left_index < list_len and nums[left_index] > nums[max_index]:
+            max_index = left_index
+        if right_index < list_len and nums[right_index] > nums[max_index]:
+            max_index = right_index
+        if max_index != parent_index:
+            nums[parent_index], nums[max_index] = nums[max_index], nums[parent_index]
+            self.heapify(nums,list_len,max_index)       
+            
+    def findKthLargest(self,nums,k):
+        list_len = len(nums)
+        for n in range(list_len//2-1,-1,-1):
+            self.heapify(nums,list_len,n) # initialize to make the parent node always larger than the sub-nodes
+        index  =1
+        if k == 1:
+            return nums[0]
+        else:
+            for n in range(list_len-1,0,-1): #to rank largest number at the lastest place for each loop by switching
+                nums[0],nums[n] = nums[n],nums[0]
+                list_len -=1
+                self.heapify(nums,list_len,0)
+                index+=1
+                if index==k:
+                    return nums[0]
+
+# Runtime: 56 ms, faster than 56.87% of Python online submissions for Kth Largest Element in an Array.
+# Memory Usage: 16 MB, less than 11.37% of Python online submissions for Kth Largest Element in an Array.
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        min_value = min(nums)
+        max_value = max(nums)
+
+        bucket_list = [[] for _ in range(min_value,max_value+1)]
+        
+        for num in nums:
+            bucket_list[num-min_value].append(num)
+
+
+        sort_list = []
+
+        for bucket in bucket_list[::-1]:
+            if len(sort_list)<k:
+                sort_list.extend(bucket)
+        
+        return sort_list[k-1]
