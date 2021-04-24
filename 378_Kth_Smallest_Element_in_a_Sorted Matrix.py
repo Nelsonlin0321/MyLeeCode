1ceab152a6b5d8d9d0f8800642d73e0a105e5319
LIN Jieqin <34851732+Nelsonlin0321@users.noreply.github.com>
2021-04-24 18:26:42 +0800

    Update 378_Kth_Smallest_Element_in_a_Sorted Matrix.py

--- 378_Kth_Smallest_Element_in_a_Sorted Matrix.py
+++ 378_Kth_Smallest_Element_in_a_Sorted Matrix.py
@@ -18,3 +18,24 @@ class Solution(object):
             i_smallest = row_num[min_index]
             matrix[min_index].remove(i_smallest)
         return i_smallest
+
+
+#Runtime: 212 ms, faster than 51.72% of Python online submissions for Kth Smallest Element in a Sorted Matrix.
+#Memory Usage: 19.1 MB, less than 75.64% of Python online submissions for Kth Smallest Element in a Sorted Matrix.   
+    
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        import heapq
+        heap = [(row[0],i,0) for (i,row) in enumerate(matrix )]
+        heapq.heapify(heap)
+        value = 0
+        for _ in range(k): # pop k times to get k_smalltest
+            value,row_i,col_j = heapq.heappop(heap)# pop the samllest value with row_i, col_j
+            if col_j+1<len(matrix[0]) : # only 0,1,2 < 3 for one row only pop the len(row) times
+                heapq.heappush(heap,(matrix[row_i][col_j+1],row_i,col_j+1))
+        return value
