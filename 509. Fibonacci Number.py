9d56a14023fa170ee0f7280491884150579deafe
LIN Jieqin <34851732+Nelsonlin0321@users.noreply.github.com>
2021-05-02 18:32:43 +0800

    Update 509. Fibonacci Number.py

--- 509. Fibonacci Number.py
+++ 509. Fibonacci Number.py
@@ -21,3 +21,25 @@ class Solution(object):
                 return result
             else:
                 return self.array[n]
+            
+# bottom up approach
+#https://leetcode.com/problems/fibonacci-number/submissions/
+#Runtime: 12 ms, faster than 95.10% of Python online submissions for Fibonacci Number.
+#Memory Usage: 13.2 MB, less than 99.01% of Python online submissions for Fibonacci Number.
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        else:
+            array = [None for _ in range(n+1)]
+            array[0] = 0
+            array[1] = 1
+            for i in range(2,n+1):
+                array[i] = array[i-1] + array[i-2]
+            return array[n]                
